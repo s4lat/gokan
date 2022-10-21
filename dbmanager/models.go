7b3bab197db12ed596b70f457f37e6b33845352f
Максим Заказчик <maksimzakazcik@s4lat.local>
2022-10-21 14:58:08 +0300

    Added 'contributor' table in db, restructured models

--- dbmanager/models.go
+++ dbmanager/models.go
@@ -1,19 +1,23 @@
 package dbmanager
 
-// Rewrite to not gorm
 type Person struct {
-	ID        uint32
-	Username  string
-	FirstName string
-	LastName  string
-	Email     string
-	PWDH      string
+	ID            uint32
+	Username      string
+	FirstName     string
+	LastName      string
+	Email         string
+	PWDH          string
+	Boards        []Board // LoadPersonBoards(), by board_id from contributor table
+	AssignedTasks []Task  // LoadPersonAssignedTasks(), from executor_id in task
 }
 
 type Board struct {
-	ID      uint32
-	Name    string
-	OwnerID uint32
+	ID           uint32
+	Name         string
+	OwnerID      uint32
+	Contributors []Person // LoadBoardContributors() by person_id from contributor table
+	Tasks        []Task   // LoadBoardTasksfrom board_id in task table
+	Tags         []Tag    // from board_id in tag table
 }
 
 type Task struct {
@@ -21,8 +25,10 @@ type Task struct {
 	Name        string
 	Description string
 	BoardID     uint32
-	AuthorID    uint32
+	Author      uint32
 	ExecutorID  uint32
+	Subtasks    []Subtask // LoadTaskSubtasks(), from parent_task_id in subtask table
+	Tags        []Tag     // always load, from task_tag table
 }
 
 type Subtask struct {
@@ -35,4 +41,5 @@ type Tag struct {
 	ID         uint32
 	Name       string
 	Descripton string
+	BoardID    uint32
 }
